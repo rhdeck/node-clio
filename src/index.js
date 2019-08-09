d4c7d6da9ac6ba97352b96332764c9d109ce8de0
Ray Deck <ray@raydeck.com>
2019-08-09 17:24:59 -0400

    Fixing the webhook creation process and allow delete to properly handle the blank response

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import fetch from "node-fetch";
-import FormData from "form-data";
 import { URLSearchParams } from "url";
 import { createHmac } from "crypto";
-const formDataArray = (formdata, key, arr) => {
-  for (const i in arr) formdata.append(key + "[]", arr[i]);
-};
+import { createWriteStream, readFile } from "fs";
+import { promisify } from "util";
 //#region Function API
 const baseUrl = "https://app.clio.com/api/v4";
 const getResult = async ret => {
+  if (ret.status === 204) return null;
   const text = await ret.text();
   try {
     const obj = JSON.parse(text);
@@ -93,23 +92,99 @@ const makeFields = fields => {
         if (typeof field === "string") return field;
         if (typeof field === "object") {
           const { field: fieldName, fields } = field;
-          const fieldString = makeFields(fields);
           return `${fieldName}{${fields}}`;
         }
       })
       .join(",")
   );
 };
-const gets = async ({ path, fields, accessToken, ...args }) => {
-  const headers = {
+const gets = async ({ path, fields, accessToken, headers, ...args }) => {
+  headers = {
+    ...(headers || {}),
     Authorization: `Bearer ${accessToken}`
   };
   const url = new URL(baseUrl);
   url.pathname = `${url.pathname}/${path}.json`;
-  url.searchParams.append("fields", makeFields(fields));
+  if (fields) url.searchParams.append("fields", makeFields(fields));
   Object.entries(args).forEach(([k, v]) => url.searchParams.append(k, v));
   const ret = await fetch(url, { method: "get", headers });
   return getResult(ret);
+};
+const bulkGetRaw = async ({
+  path,
+  fields,
+  accessToken,
+  onProgress,
+  ...args
+}) => {
+  const headers = {
+    "X-BULK": "true",
+    Authorization: `Bearer ${accessToken}`
+  };
+  //Kick off the bulk fetch
+  const url = new URL(baseUrl);
+  url.pathname = `${url.pathname}/${path}.json`;
+  if (fields) url.searchParams.append("fields", makeFields(fields));
+  Object.entries(args).forEach(([k, v]) => url.searchParams.append(k, v));
+  const ret = await fetch(url, { method: "get", headers });
+
+  const pollCheckURL = ret.headers.get("Location");
+  const doCheck = true;
+  while (doCheck) {
+    const ret = await fetch(pollCheckURL, { method: "get", headers });
+    switch (ret.code) {
+      case 200:
+        //Nothing to do
+        if (onProgress) {
+          const text = await ret.text();
+          const obj = JSON.parse(text);
+          await onProgress(obj);
+        }
+        break;
+      case 303:
+        //Woot!
+        return fetch(ret.headers.get("Location"));
+    }
+  }
+  throw "Failed to download bulk";
+};
+const bulkGetText = async ({
+  path,
+  fields,
+  accessToken,
+  onProgress,
+  outPath
+}) => {
+  if (outPath) {
+    await bulkGetFile({ path, fields, accessToken, onProgress, outPath });
+    return await promisify(readFile)(outPath);
+  } else {
+    const ret = await bulkGetRaw({ path, fields, accessToken, onProgress });
+    return await ret.text();
+  }
+};
+const bulkGetFile = async ({
+  path,
+  fields,
+  accessToken,
+  onProgress,
+  outPath
+}) => {
+  const ret = await bulkGetRaw({ path, fields, accessToken, onProgress });
+  await new Promise(r => {
+    ret.body.pipe(createWriteStream(outPath));
+    ret.body.on("end", () => r());
+  });
+};
+const bulkGetObj = async ({
+  path,
+  fields,
+  accessToken,
+  onProgress,
+  outPath
+}) => {
+  const text = bulkGetText({ path, fields, accessToken, onProgress, outPath });
+  return JSON.parse(text);
 };
 const makeWebHook = async ({
   url,
@@ -120,25 +195,35 @@ const makeWebHook = async ({
   accessToken
 }) => {
   const headers = {
-    Authorization: `Bearer ${accessToken}`
+    Authorization: `Bearer ${accessToken}`,
+    ["Content-Type"]: "application/json"
   };
   const whUrl = new URL("https://app.clio.com/api/v4/webhooks.json");
-  const body = { model };
+  if (!model) throw "Model is required";
+  const data = { model };
   if (!url) throw "url is required";
-  body.url = url;
-  if (events) body.events = events;
-  if (fields) body.fields = makeFields(fields);
+  data.url = url;
+  if (events) data.events = events;
+  if (!fields) throw "fields are required";
+  data.fields = makeFields(fields);
   if (expires && !(expires instanceof Date)) expires = new Date(expires);
-  if (expires) body.expires_at = expires.toISOString();
-
+  if (expires) data.expires_at = expires.toISOString();
+  const bodyObj = { data };
+  const body = JSON.stringify(bodyObj);
   const ret = await fetch(whUrl, {
     method: "post",
     headers,
-    body: JSON.stringify(body)
+    body
   });
   const text = await ret.text();
   try {
     const obj = JSON.parse(text);
+    if (obj.error) {
+      //Oh noes
+      throw obj;
+    }
+    console.log("I am giving you object now!!!");
+    console.log(text);
     return obj;
   } catch (e) {
     console.log(
@@ -349,16 +434,25 @@ class Clio {
     };
   }
   async map({ path, fields, ...args }, f) {
+    if (!path) throw "Path is required for map";
     let obj = await this.getPage({ path, fields, ...args });
+    let out = [];
     while (obj) {
       const { page, getNext } = obj;
-      await Promise.all(page.map(f));
+      const temp = await Promise.all(page.map(f));
+      out = [...out, ...temp];
       if (getNext) obj = await getNext();
       else obj = null;
     }
+    return out;
+  }
+  async getAll({ path, fields, ...args }) {
+    console.log("Getting all for ", path);
+    if (!path) throw "Path is required for getAll";
+    return this.map({ path, fields, ...args }, o => o);
   }
   async mapEntities({ path, fields }, f) {
-    getNextPage = await getPageEntities({ path, fields });
+    let getNextPage = await this.getPageEntities({ path, fields });
     while (getNextPage) {
       const { page, getNext } = getNextPage();
       await Promise.all(page.map(f));
@@ -392,14 +486,13 @@ class Clio {
   }
   async withEntities({ page, getNext, getPrevious, path, fields }) {
     //convert page elements to entities
-    const entities = page.map(properties => {
-      const entity = new ClioEntity(this, {
+    const entities = page.map(properties =>
+      this.makeEntity(this, {
         fields,
         properties,
         type: path
-      });
-      return entity;
-    });
+      })
+    );
     return {
       page: entities,
       raw: page,
@@ -438,6 +531,9 @@ class Clio {
   }
   async makeCustomAction({ label, targetUrl, uiReference, type }) {
     if (type) uiReference = `${type}/show`;
+    if (!uiReference) throw "uiReference is required";
+    if (!label) throw "label is required";
+    if (!targetUrl) throw "targeUrl is required";
     const {
       data: {
         id,
@@ -486,6 +582,24 @@ class Clio {
       accessToken
     });
   }
+  async makeEntity({ type, id, fields, properties }) {
+    return new ClioEntity(this, { type, id, fields, properties });
+  }
+  async clear(path) {
+    console.log("I am starting clear", path);
+    const os = await this.getAll({ path });
+    console.log("I got my os");
+    console.log(os);
+    for (const { id } of os) {
+      console.log("Removing", id);
+      try {
+        await this.remove({ path, id });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    console.log("Done with clear");
+  }
 }
 //#endregion
 //#region ClioEntity Class
@@ -494,15 +608,26 @@ class ClioEntity {
     this.clio = clio;
     this.properties = properties;
     if (fields) this.fields = fields;
-    this.type = path;
+    this.type = type;
     this.id = id ? id : properties.id;
     this.etag = etag ? etag : properties.etag;
+  }
+  async load() {
+    const { etag, ...properties } = await this.clio.get({
+      type: this.type,
+      id: this.id,
+      fields: this.fields
+    });
+    this.etag = etag;
+    this.properties = properties;
+    return this;
   }
   async update(changes) {
     const ret = await this.clio.update({
       path: this.type,
       id: this.id,
-      data: changes
+      data: changes,
+      etag: this.etag
     });
     this.properties = { ...this.properties, ret };
     this.etag = ret.etag;
